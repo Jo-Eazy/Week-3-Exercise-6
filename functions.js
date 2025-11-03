e1b89c8a084f09da56e9c2a7a8c132fc81d7ba12
Jo-Eazy <josedhlamini9@gmail.com>
2025-11-03 15:01:38 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+const greeting = greet("Jose");
+const sum = add(6, 9);
+
+console.log(greeting);
+console.log("The sum of 6 and 9 is:", sum);
